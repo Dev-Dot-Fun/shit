5fa03c4f8df9dacd279e9a20e339fb89ce5cc568
DjonniStorm <i.pazushkin@yandex.ru>
2024-10-26 13:52:54 +0400

    base starting

--- example/obsl/app/index.tsx
+++ example/obsl/app/index.tsx
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import { AvatarIcon } from '../assets/icons/avatar-icon';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>
+        Open up App.tsx to start working on your
+        app!
+      </Text>
       <StatusBar style="auto" />
+      <AvatarIcon
+        width={100}
+        height={100}
+        color={'#e8eaed'}
+      />
     </View>
   );
 }
---
+++ example/obsl/assets/icons/menu-icon.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import Svg, { Path } from 'react-native-svg';
+import { IconProps } from './icon.props';
+export const MenuIcon = ({
+  width,
+  height,
+  color,
+}: IconProps) => (
+  <Svg
+    width={width}
+    height={height}
+    fill={color}
+    viewBox="0 -960 960 960">
+    <Path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z" />
+  </Svg>
+);
